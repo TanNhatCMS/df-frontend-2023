3da10717d7c809655e470fcb1b0adf75dbbb9336
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2023-09-14 19:05:38 +0700

    Update script.js

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,62 +1,56 @@
-// Your JS code goes here
-const bookList = document.getElementById("book-list");
-const addBookForm = document.getElementById("add-book-form");
-const bookTable = document.getElementById("book-table");
-const searchInput = document.getElementById("search-input");
+// Lấy dữ liệu từ localStorage (nếu có) khi trang được nạp
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
-// Retrieve data from localStorage or initialize it
-let books = JSON.parse(localStorage.getItem("books")) || [];
-
-// Function to save data to localStorage
-function saveDataToLocalStorage() {
-    localStorage.setItem("books", JSON.stringify(books));
-}
-
-// Display books in the table
-function displayBooks(booksToDisplay = books) {
-    bookList.innerHTML = "";
-    for (let i = 0; i < booksToDisplay.length; i++) {
-        const book = booksToDisplay[i];
-        const row = bookTable.insertRow();
-        const cell1 = row.insertCell(0);
-        const cell2 = row.insertCell(1);
-        const cell3 = row.insertCell(2);
-        const cell4 = row.insertCell(3);
-
-        cell1.innerHTML = book.title;
-        cell2.innerHTML = book.author;
-        cell3.innerHTML = book.topic;
-        cell4.innerHTML = `<button onclick="deleteBook(${i})">Delete</button>`;
+// Cập nhật bảng sách
+function updateBookList() {
+    const bookList = document.getElementById('bookList');
+    bookList.innerHTML = '';
+    for (let i = 0; i < books.length; i++) {
+        const book = books[i];
+        const row = `
+            <tr>
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.topic}</td>
+                <td><button onclick="deleteBook(${i})">Xóa</button></td>
+            </tr>
+        `;
+        bookList.innerHTML += row;
     }
-    saveDataToLocalStorage();
 }
 
-// Delete a book
-function deleteBook(index) {
-    books.splice(index, 1);
-    displayBooks();
-}
-
-// Add a new book
-addBookForm.addEventListener("submit", function (e) {
+// Thêm sách mới
+const addBookForm = document.getElementById('addBookForm');
+addBookForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const topic = document.getElementById("topic").value;
-
-    books.push({ title, author, topic });
-    displayBooks();
+    const title = document.getElementById('bookName').value;
+    const author = document.getElementById('author').value;
+    const topic = document.getElementById('topic').value;
+    const newBook = { title, author, topic };
+    books.push(newBook);
+    localStorage.setItem('books', JSON.stringify(books));
+    updateBookList();
     addBookForm.reset();
 });
 
-// Search books
-searchInput.addEventListener("input", function () {
-    const query = searchInput.value.toLowerCase();
+// Xóa sách
+function deleteBook(index) {
+    books.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(books));
+    updateBookList();
+}
+
+// Tìm kiếm sách
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
     const filteredBooks = books.filter(book =>
-        book.title.toLowerCase().includes(query)
+        book.title.toLowerCase().includes(searchTerm) ||
+        book.author.toLowerCase().includes(searchTerm) ||
+        book.topic.toLowerCase().includes(searchTerm)
     );
-    displayBooks(filteredBooks);
+    updateBookList(filteredBooks);
 });
 
-// Display the initial list of books
-displayBooks();
+// Cập nhật bảng khi trang được nạp
+updateBookList();
