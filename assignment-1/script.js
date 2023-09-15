17f5d8d5dc54aa2f35bd3a3807ed82fe77becda1
TanNhatCMS <mrkiet.edu@gmail.com>
2023-09-15 13:15:22 +0700

    update language

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,28 +1,91 @@
-
-let books = [
-    { title: "Cuốn Sách 1", author: "Tác giả A", topic: "Chủ đề X" },
-    { title: "Cuốn Sách 2", author: "Tác giả B", topic: "Chủ đề Y" }
-];
-
-
 const bookList = document.getElementById("book-list");
 const addBookForm = document.getElementById("add-book-form");
 const searchBookForm = document.getElementById("search-book-form");
-const titleInput = document.getElementById("title");
+const nameInput = document.getElementById("name");
 const authorInput = document.getElementById("author");
 const topicInput = document.getElementById("topic");
 const searchInput = document.getElementById("search");
 const deleteConfirmationModal = document.getElementById("deleteConfirmationModal");
 const confirmDeleteButton = document.getElementById("confirmDeleteButton");
 const cancelDeleteButton = document.getElementById("cancelDeleteButton");
+const addBookModal = document.getElementById("addBookModal");
+const addBookButton = document.getElementById("addBookButton");
+const closeAddBookModal = document.getElementById("closeAddBookModal");
+const topicSelect = document.getElementById("topic");
+
+const languageText = {
+    english: {
+        addBook: "Add New Book",
+        confirmDelete: "Are you sure you want to delete this book?",
+        addButton: "Add",
+        cancelDeleteButton: "Cancel",
+        searchPlaceholder: "Search by name",
+        modalname: "Add New Book",
+        nameLabel: "name",
+        authorLabel: "Author",
+        topicLabel: "Topic",
+        actionLabel: "Action",
+        confirmButton: "Confirm",
+        closeButton: "Close",
+    },
+    vietnamese: {
+        addBook: "Thêm Sách Mới",
+        confirmDelete: "Bạn có chắc chắn muốn xóa cuốn sách này?",
+        addButton: "Thêm",
+        cancelDeleteButton: "Hủy",
+        searchPlaceholder: "Tìm theo Tiêu đề",
+        modalname: "Thêm Sách Mới",
+        nameLabel: "Tiêu đề",
+        authorLabel: "Tác giả",
+        topicLabel: "Chủ đề",
+        actionLabel: "Thao tác",
+        confirmButton: "Xác nhận",
+        closeButton: "Đóng",
+    },
+};
+
+let currentLanguage = "english";
 let confirmDeleteIndex = null;
+let books = [
+    { name: "Refactoring", author: "Martin Fowler", topic: "Programming" },
+    { name: "Designing Data-intene Applications", author: "Martin Kleppmann", topic: "Database" },
+    { name: "The Phoenix Project", author: "Gene Kim", topic: "DevOps" }
+
+];
+
+books.forEach(book => {
+    const option = document.createElement("option");
+    option.value = book.topic;
+    option.text = book.topic;
+    topicSelect.appendChild(option);
+});
+
+function updateTextByLanguage() {
+    const elementsToChange = document.querySelectorAll("[data-i18n]");
+    elementsToChange.forEach(element => {
+        const key = element.getAttribute("data-i18n");
+        element.textContent = languageText[currentLanguage][key];
+    });
+}
+
+updateTextByLanguage();
+
+document.getElementById("english-button").addEventListener("click", function () {
+    currentLanguage = "english";
+    updateTextByLanguage();
+});
+
+document.getElementById("vietnamese-button").addEventListener("click", function () {
+    currentLanguage = "vietnamese";
+    updateTextByLanguage();
+});
 
 function displayBooks() {
     bookList.innerHTML = "";
     for (let i = 0; i < books.length; i++) {
         const row = document.createElement("tr");
         row.innerHTML = `
-                <td>${books[i].title}</td>
+                <td>${books[i].name}</td>
                 <td>${books[i].author}</td>
                 <td>${books[i].topic}</td>
                 <td><button onclick="showDeleteConfirmation(${i})">Xóa</button></td>
@@ -35,7 +98,7 @@ function displayBooks() {
 addBookForm.addEventListener("submit", function (e) {
     e.preventDefault();
     const newBook = {
-        title: titleInput.value,
+        name: nameInput.value,
         author: authorInput.value,
         topic: topicInput.value
     };
@@ -60,18 +123,14 @@ confirmDeleteButton.addEventListener("click", function () {
         hideDeleteConfirmation();
     }
 });
+
 cancelDeleteButton.addEventListener("click", function () {
     hideDeleteConfirmation();
 });
 
 function deleteBook(index) {
-
-    const isConfirmed = confirm("Bạn có chắc chắn muốn xóa cuốn sách này?");
-
-    if (isConfirmed) {
-        books.splice(index, 1);
-        displayBooks();
-    }
+    books.splice(index, 1);
+    displayBooks();
 }
 
 function redisplayBooks(booksToDisplay) {
@@ -79,7 +138,7 @@ function redisplayBooks(booksToDisplay) {
     for (let i = 0; i < booksToDisplay.length; i++) {
         const row = document.createElement("tr");
         row.innerHTML = `
-            <td>${booksToDisplay[i].title}</td>
+            <td>${booksToDisplay[i].name}</td>
             <td>${booksToDisplay[i].author}</td>
             <td>${booksToDisplay[i].topic}</td>
             <td><button onclick="deleteBook(${i})">Xóa</button></td>
@@ -97,7 +156,7 @@ searchBookForm.addEventListener("submit", function (e) {
 });
 
 function searchBooks(query) {
-    const results = books.filter(book => book.title.toLowerCase().includes(query.toLowerCase()));
+    const results = books.filter(book => book.name.toLowerCase().includes(query.toLowerCase()));
     redisplayBooks(results);
 }
 
