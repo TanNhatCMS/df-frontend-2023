9278e21606c4c5e8e33d7cfe64bf946b3b4b32b9
TanNhatCMS <mrkiet.edu@gmail.com>
2023-09-23 16:50:39 +0700

    update

--- assignment-2/src/view/layouts/Header/Header.js
+++ assignment-2/src/view/layouts/Header/Header.js
@@ -1,64 +1,23 @@
-import React, { useContext, useState } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import Theme from "../../components/Theme/Theme";
 import Account from "../../components/Account/Account";
-import LanguageSwitcher from "../../components/LanguageSwitcher/LanguageSwitcher";
-import MobileNavMenu from "../../components/MobileNavMenu/MobileNavMenu";
-
 import "./Header.css";
 
 const Header = ({ currentLanguage, onLanguageChange }) => {
-  const [showMenu, setShowMenu] = useState(false);
-
-
-  function toggleMenu() {
-    setShowMenu(!showMenu);
-  }
   const { theme } = useContext(ThemeContext);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-
-  const openMobileMenu = () => {
-    setIsMobileMenuOpen(true);
-  };
-
-
-  const closeMobileMenu = () => {
-    setIsMobileMenuOpen(false);
-  };
 
   return (
-    <header id="header" className={`header-${theme}`}>
-      <a data-i18n="titleSite" href="#" className="logo">Bookstore</a>
-      <div className="user-info">
-        <Theme />
-        <img className="user-avatar" src="./assets/images/user.png" alt="Avatar người dùng" />
-        <h1 className="user-name">Tân Nhật CMS</h1>
-
-      </div>
-
-
-
-      {/* <div className="header-logo">
+    <header id="header" className={`theme-${theme}`}>
+      <div className="header-logo">
         <a href="/">Bookstore</a>
       </div>
 
       <div className="header-actions">
-        <button className="mobile-menu-button" onClick={openMobileMenu}>
-          Menu
-        </button>
-       
-        <Theme />
-        <Account
-          currentLanguage={currentLanguage}
-          onLanguageChange={onLanguageChange}
-        />
-      </div>*/}
 
-      {/* Render MobileNavMenu nếu trạng thái mở Mobile Navigation Menu là true*/}
-      {isMobileMenuOpen && (
-        <MobileNavMenu isOpen={isMobileMenuOpen} onClose={closeMobileMenu} />
-      )}
+        <Theme />
+        <Account currentLanguage={currentLanguage} onLanguageChange={onLanguageChange} />
+      </div>
     </header>
   );
 };
--- assignment-2/src/view/layouts/Main/Main.js
+++ assignment-2/src/view/layouts/Main/Main.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import Header from "../Header/Header";
 import "./Main.css";
-
+import LanguageSwitcher from '../../components/LanguageSwitcher/LanguageSwitcher';
 const Main = ({ currentLanguage, onLanguageChange, children }) => {
   const { theme } = useContext(ThemeContext);
 
@@ -10,7 +10,17 @@ const Main = ({ currentLanguage, onLanguageChange, children }) => {
     <> <Header currentLanguage={currentLanguage} onLanguageChange={onLanguageChange} ></Header>
       <main id="main" className={`theme-${theme}`}>
         {children}
+
       </main>
+      <LanguageSwitcher currentLanguage={currentLanguage} onLanguageChange={onLanguageChange} />
+      <footer>
+
+        <div className="copyright">
+          &copy; 2023 || Made with TânNhậtCMS||
+          <a href="https://github.com/TanNhatCMS/df-frontend-2023/tree/main/assignment-1">Source code</a>
+        </div>
+
+      </footer>
     </>
   );
 };
