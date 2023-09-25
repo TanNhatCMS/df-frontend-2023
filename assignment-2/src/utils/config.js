181919c88dbdd382d6d976fc5d6f5134d8066438
TanNhatCMS <mrkiet.edu@gmail.com>
2023-09-26 01:23:49 +0700

    Create config.js

---
+++ assignment-2/src/utils/config.js
@@ -0,0 +1,18 @@
+const STYLES = {
+  light: {
+    text: 'hsl(0deg, 0%, 10%)',
+    background: 'hsl(0deg, 0%, 100%)',
+    primary: '#e23c5f',
+    toggle: 'hsl(250deg, 70%, 7%)',
+  },
+  dark: {
+    text: 'hsl(0deg, 0%, 100%)',
+    background: 'hsl(250deg, 70%, 7%)',
+    primary: '#e23c5f',
+    toggle: 'hsl(0deg, 0%, 100%)',
+  },
+}
+
+const BOOKS_PAGE_ITEM = 5
+
+export { STYLES, BOOKS_PAGE_ITEM }
