318bf4c697004249854b0ed8531c0d4ffb6b4e16
TanNhatCMS <mrkiet.edu@gmail.com>
2023-09-26 02:20:34 +0700

    Update Account.js

--- assignment-2/src/view/components/Account/Account.js
+++ assignment-2/src/view/components/Account/Account.js
@@ -1,12 +1,13 @@
-import { useContext } from "react";
-import { ThemeContext } from "../../contexts/ThemeContext";
+import { useState } from "react";
+import { getThemeMode } from '../../../utils/functions'
 import styles from "./Account.module.css";
+import IMAGES from '../../../assets/images'
 const Account = () => {
-  const { theme } = useContext(ThemeContext);
+  const { theme } = useState(getThemeMode);
   return (
     <div className={`${styles["account"]} ${styles[`theme-${theme}`]}`}>
       <div className="account-image">
-        <img className="account-avatar" src="./assets/images/user.png" alt="Avatar người dùng" />
+        <img className="account-avatar" src={IMAGES.user} alt="Avatar người dùng" />
       </div>
       <div className="account-name">
         <span>TânNhậtCMS</span>
